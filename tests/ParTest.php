ebd151e6767f26b7ac60aef460f5de19c2d34f84
Pilar Melió <mariap.melio@gmail.com>
2018-10-28 12:56:13 +0100

    varios test

--- tests/ParTest.php
+++ tests/ParTest.php
@@ -16,9 +16,19 @@ class PaTests extends TestCase
         $this->Par = NULL;
     }
  
-    public function testAdd()
+    public function testAddCero()
+    {
+        $result = $this->par->esPar(0);
+        $this->assertEquals(true, $result);
+    }
+	  public function testAddPar()
     {
         $result = $this->par->esPar(4);
+        $this->assertEquals(true, $result);
+    }
+	  public function testAddImpar()
+    {
+        $result = $this->par->esPar(5);
         $this->assertEquals(true, $result);
     }
 
